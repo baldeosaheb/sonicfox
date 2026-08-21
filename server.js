57861907340154fd38f9ca622f2d3b87e1ac6ea3
Ravi Kumar <rganga757@gmail.com>
2026-08-21 12:24:52 +0530

    Support both EMAIL_PASS and EMAIL_PASSWORD env variable names

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ const transporter = nodemailer.createTransport({
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    pass: process.env.EMAIL_PASS || process.env.EMAIL_PASSWORD
   },
   connectionTimeout: 10000,  // 10s to connect
   greetingTimeout: 10000,    // 10s for greeting
